c31a4c8ae7149c057fffa2d1b639cf02091be2e4
Alexandar Valov <alexvulov123@gmail.com>
2024-11-15 10:11:40 +0200

    Login Improvements
    Redid the whole page using only Vuetify 3 components and utility classes.

--- pages/portal-mockup/login.vue
+++ pages/portal-mockup/login.vue
@@ -1,16 +1,15 @@
-<script setup>
-import { ref, onMounted } from "vue";
-import { useRouter } from "vue-router";
-
+<script setup lang="ts">
+// Props
 definePageMeta({ layout: "login" });
 
+// State for form fields and control variables
 const router = useRouter();
-const userId = ref("");
-const password = ref("");
-const visible = ref(true);
-const rememberMe = ref(false);
+const userId = ref<string>("");
+const password = ref<string>("");
+const visible = ref<boolean>(true);
+const rememberMe = ref<boolean>(false);
 
-// Check local storage for remembered userId
+// Retrieve remembered userId from localStorage on page load
 onMounted(() => {
   const savedUserId = localStorage.getItem("rememberedUserId");
   if (savedUserId) {
@@ -19,6 +18,7 @@ onMounted(() => {
   }
 });
 
+// Function that handles login functionality
 const handleLogin = () => {
   localStorage.setItem("isLoggedIn", "true");
   rememberMe.value
@@ -27,12 +27,13 @@ const handleLogin = () => {
   router.push("/portal-mockup");
 };
 
-const toggleVisibility = (field) => {
-  if (field === "user") visible.value = !visible.value;
-  else visible.value = !visible.value;
+// Function that toggles password visibility
+const toggleVisibility = (field: string) => {
+  visible.value = !visible.value;
 };
 
-const appendInnerIcon = (field) =>
+// Function that selects appropriate icon for user and password fields
+const appendInnerIcon = (field: string) =>
   field === "user"
     ? "mdi-account-outline"
     : visible.value
@@ -41,54 +42,79 @@ const appendInnerIcon = (field) =>
 </script>
 
 <template>
-  <v-container class="login-container">
-    <v-row justify="center" align="center">
+  <!-- Main container -->
+  <v-container
+    class="d-flex align-center justify-center"
+    style="height: calc(80vh - 140px)"
+  >
+    <v-row justify="center">
       <v-col cols="12" sm="8" md="6" lg="4">
-        <v-card class="login-card" rounded="xl" elevation="2">
-          <v-card-title class="text-center title">Welcome back!</v-card-title>
+        <!-- Card -->
+        <v-card
+          rounded="lg"
+          elevation="1"
+          class="px-4 py-6 bg-white mx-auto"
+          max-width="400"
+        >
+          <v-card-title
+            class="text-center text-h4 text-teal font-weight-bold mb-4"
+            >Welcome back!</v-card-title
+          >
           <v-card-text>
-            <v-form @submit.prevent="handleLogin" class="login-form">
+            <!-- Form -->
+            <v-form @submit.prevent="handleLogin">
+              <!-- User ID -->
               <v-text-field
                 v-model="userId"
                 label="User ID"
                 type="email"
                 variant="outlined"
                 placeholder="1-2125972"
-                density="comfortable"
+                density="compact"
                 rounded="lg"
-                class="input-field"
                 :append-inner-icon="appendInnerIcon('user')"
                 @click:append-inner="toggleVisibility('user')"
               ></v-text-field>
+
+              <!-- Password -->
               <v-text-field
                 v-model="password"
                 label="Password"
                 :type="visible ? 'password' : 'text'"
                 variant="outlined"
                 placeholder="Enter your password"
-                density="comfortable"
+                density="compact"
                 rounded="lg"
                 :append-inner-icon="appendInnerIcon('password')"
                 @click:append-inner="toggleVisibility('password')"
               ></v-text-field>
+
+              <!-- "Remember me" checkbox -->
               <v-checkbox
                 v-model="rememberMe"
-                :style="{ color: '#085d4f', scale: 0.8 }"
                 label="Remember me"
                 hide-details
+                class="mt-n4 mb-2"
               ></v-checkbox>
+
+              <!-- Submit button -->
               <v-btn
                 type="submit"
                 variant="outlined"
+                color="teal"
                 rounded="lg"
-                class="submit-btn"
+                class="w-100 font-weight-bold"
               >
                 Sign In
               </v-btn>
             </v-form>
-            <div class="signup-prompt">
+
+            <!-- Signup link-->
+            <div class="mt-4 text-center text-body-2 text-teal">
               <span>Don't have an account yet? </span>
-              <span class="signup-link">Sign Up</span>
+              <span class="font-weight-bold text-decoration-none cursor-pointer"
+                >Sign Up</span
+              >
             </div>
           </v-card-text>
         </v-card>
@@ -96,62 +122,3 @@ const appendInnerIcon = (field) =>
     </v-row>
   </v-container>
 </template>
-
-<style scoped lang="scss">
-.login-container {
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 20px 40px;
-
-  .login-card {
-    padding: 40px 20px;
-
-    .title {
-      font-size: 28px;
-      font-weight: bold;
-      color: #085d4f;
-    }
-
-    .login-form {
-      display: flex;
-      flex-direction: column;
-      margin-top: 20px;
-
-      .v-checkbox {
-        margin-top: -20px;
-        margin-bottom: 10px;
-        margin-left: -30px;
-      }
-
-      .submit-btn {
-        width: 100%;
-        color: white;
-        background-color: #085d4f;
-        font-weight: bold;
-        height: 48px;
-        transition: background-color 0.3s ease;
-
-        &:hover {
-          background-color: white;
-          color: #085d4f;
-        }
-      }
-    }
-
-    .signup-prompt {
-      margin-top: 20px;
-      text-align: center;
-      color: #085d4f;
-
-      .signup-link {
-        color: #085d4f;
-        font-weight: bold;
-        cursor: pointer;
-        text-decoration: underline;
-      }
-    }
-  }
-}
-</style>
